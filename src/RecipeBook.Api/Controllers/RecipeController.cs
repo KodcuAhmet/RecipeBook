8b93060177ecb1783d9d2f30a33289707e0e2919
Ahmet <57807015+ahmet-cigil@users.noreply.github.com>
2024-07-12 15:47:38 +0200

    Add project files.

---
+++ src/RecipeBook.Api/Controllers/RecipeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecipeBook.ServiceLibrary.Domains;
+using RecipeBook.ServiceLibrary.Entities;
+
+namespace RecipeBook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpGet("{recipeId}")] //api/recipe/{recipeId}
+        public async Task<IActionResult> GetOnceAsync(
+            [FromRoute]Guid recipeId)
+        {
+            return Ok(recipeId);
+        }
+
+        [HttpGet] // api/recipe?pageSize=10&pageNumber=1
+        public async Task<IActionResult> GetListAsync(
+            [FromQuery]int pageSize,
+            [FromQuery]int pageNumber)
+        {
+            return Ok(pageSize + " " + pageNumber);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(
+            [FromBody] RecipeEntity recipe)
+        {
+            return Ok(recipe);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync([FromBody] RecipeEntity recipe)
+        {
+            return Ok(recipe);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(Guid recipeId)
+        {
+            return Ok(recipeId);
+        }
+    }
+}
